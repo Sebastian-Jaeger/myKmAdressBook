5f87078ccae89af281f17cba86634d4ddc734616
Sebastian Jäger <Sebastian Jger>
2016-10-21 16:15:08 +0200

    Datenbank und Zugriffsklassen hinzugefügt #5

--- myKmAdressBook/Controllers/HomeController.cs
+++ myKmAdressBook/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using myKmAdressBook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,11 @@ namespace myKmAdressBook.Controllers
 {
     public class HomeController : Controller
     {
+        private MyDbContext db = new MyDbContext(); 
+
         public ActionResult Index()
         {
+
             return View();
         }
 
--- myKmAdressBook/Models/Adress.cs
+++ myKmAdressBook/Models/Adress.cs
@@ -1,13 +1,33 @@
-﻿namespace myKmAdressBook.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace myKmAdressBook.Models
 {
     public class Adress
     {
         public int Id { get; set; }
+
+        [Display(Name="Kundennummer")]
+        [Required]
         public string CustumerId { get; set; }
+
+        [Display(Name="Kundenname")]
+        [Required]
         public string Name { get; set; }
+
+        [Display(Name="Postleitzahl")]
+        [Required]
         public string Postcode { get; set;  }
+
+        [Display(Name="Ort")]
+        [Required]
         public string City { get; set; }
+
+        [Display(Name="Land")]
+        [Required]
         public string Country { get; set; }
+
+        [Display(Name="Straße")]
+        [Required]
         public string Street { get; set; }
 
     }

20a0c751464883ef56d43fb8a6a8310797fc2c8f
Sebastian Jäger <Sebastian Jger>
2016-10-21 08:24:02 +0200

    Projektsetup

---
+++ myKmAdressBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myKmAdressBook.Startup))]
+namespace myKmAdressBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

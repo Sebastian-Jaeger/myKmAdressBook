b3694df1c88e1ab9288f436f033499c091cf69b3
Sebastian Jäger <Sebastian Jger>
2016-10-21 14:48:34 +0200

    Erstellung des Datentransfer Models #7

---
+++ myKmAdressBook/Models/Adress.cs
@@ -0,0 +1,14 @@
+﻿namespace myKmAdressBook.Models
+{
+    public class Adress
+    {
+        public int Id { get; set; }
+        public string CustumerId { get; set; }
+        public string Name { get; set; }
+        public string Postcode { get; set;  }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Street { get; set; }
+
+    }
+}
